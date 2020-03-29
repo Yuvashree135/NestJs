b513168ee1cabd2248a7816f29ada9dfc9c422d6
Yuvashree135 <syuvashree1995@gmail.com>
2020-03-29 21:14:28 +0530

    added basic http types

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessageModel } from './message.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('name/:name')
+  getParamValue(@Param() params): string {
+    return this.appService.getWelcomeMessage(params.name);
+  }
+
+  @Get('values')
+  getRequestQuery(@Query() query: MessageModel): string {
+    return query.name;
+  }
+
+  @Post('postMessage')
+  getPostMessage(@Body() message: MessageModel): string {
+    return message.name;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  getWelcomeMessage(name: string): string {
+    return 'Hello '+ name;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
